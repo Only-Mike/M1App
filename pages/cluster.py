e04ecdcb6a8f84f7157bc8a2c9ec832ac5339fca
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-10-04 20:07:38 +0200

    f

--- pages/cluster.py
+++ pages/cluster.py
@@ -1,6 +1,23 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import fancyimpute as fi
+import shap
+import pickle
+import itertools
+import imblearn
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
 
 
 st.set_page_config(page_title='Streamlit Cluster', layout = 'wide')
 st.header(' Graphs showing clusters in HR data')
 st.write('hello world')
+
+
+
