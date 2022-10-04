0a804d326bfcb21cc2a3cdc38e70d9af599cba29
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-10-04 20:11:39 +0200

    f

--- pages/cluster.py
+++ pages/cluster.py
@@ -11,13 +11,17 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.decomposition import PCA
 
-
+#Set page layout/header
 st.set_page_config(page_title='Streamlit Cluster', layout = 'wide')
 st.header(' Graphs showing clusters in HR data')
-st.write('hello world')
 
+#Import datasets
 hr = pd.read_csv("data_dictionary.csv")
 hr1 = pd.read_csv("employee_survey_data.csv")
 hr2 = pd.read_csv("general_data.csv")
 hr3 = pd.read_csv("manager_survey_data.csv")
+
+# Merge datasets
+mergehr = pd.merge(hr1, hr2)
+hr_df = pd.merge(mergehr, hr3)
 
