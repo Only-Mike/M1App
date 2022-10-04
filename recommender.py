2f4343c88867c2f2efc1e6228d3f1b4807edfa00
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-10-04 19:35:14 +0200

    f

--- recommender.py
+++ recommender.py
@@ -32,29 +32,29 @@ def read_process_data():
 
     # encode ids
     le_Department = LabelEncoder()
-    le_Age = LabelEncoder()
+    le_EducationField = LabelEncoder()
 
     hr_df['Department'] = le_Department.fit_transform(hr_df['Department'])
-    hr_df['Age'] = le_Age.fit_transform(hr_df['Age'])
+    hr_df['EducationField'] = le_EducationField.fit_transform(hr_df['EducationField'])
 
 
     # construct matrix
     ones = np.ones(len(hr_df), np.uint32)
-    matrix = ss.coo_matrix((ones, (hr_df['Department'], hr_df['Age'])))
+    matrix = ss.coo_matrix((ones, (hr_df['Department'], hr_df['EducationField'])))
 
     # decomposition
     svd = TruncatedSVD(n_components=5, n_iter=7, random_state=42)
     matrix_Department = svd.fit_transform(matrix)
-    matrix_Age = svd.fit_transform(matrix.T)
+    matrix_EducationField = svd.fit_transform(matrix.T)
 
 
     # distance-matrix
     cosine_distance_matrix_Department = cosine_distances(matrix_Department)
   
 
-    return hr_df, le_Department, le_Age, matrix, svd, matrix_Department, matrix_Age, cosine_distance_matrix_Department
+    return hr_df, le_Department, le_EducationField, matrix, svd, matrix_Department, matrix_EducationField, cosine_distance_matrix_Department
 
-hr_df, le_Department, le_Age, matrix, svd, matrix_Department, matrix_Age, cosine_distance_matrix_Department = read_process_data()
+hr_df, le_Department, le_EducationField, matrix, svd, matrix_Department, matrix_EducationField, cosine_distance_matrix_Department = read_process_data()
 
 
 def similar_Department(Department, n):
@@ -76,21 +76,21 @@ if st.button('Recommend Something - click!'):
     st.write(similar_Department(department, n_recs_c))
 
 
-def similar_Department_Age(Age, n):
-  u_id = le_Age.transform([Age])[0]
-  Age_ids = hr_df[hr_df.Age == u_id]['Department'].unique()
-  Age_vector_hr_df = np.mean(matrix_Department[Age_ids], axis=0)
-  closest_for_user = cosine_distances(Age_vector_hr_df.reshape(1,5), matrix_Department)
+def similar_Department_EducationField(EducationField, n):
+  u_id = le_EducationField.transform([EducationField])[0]
+  EducationField_ids = hr_df[hr_df.EducationField == u_id]['Department'].unique()
+  EducationField_vector_hr_df = np.mean(matrix_Department[EducationField_ids], axis=0)
+  closest_for_user = cosine_distances(EducationField_vector_hr_df.reshape(1,5), matrix_Department)
   sim_Department = le_Department.inverse_transform(np.argsort(closest_for_user[0])[:n])
   return sim_Department
 
-one_user = st.selectbox('Select Age', hr_df.Age.unique())
+one_user = st.selectbox('Select EducationField', hr_df.EducationField.unique())
 if one_user:
-    st.write(hr_df[hr_df.Age == one_user]['Age'].unique())
+    st.write(hr_df[hr_df.EducationField == one_user]['EducationField'].unique())
 
-n_recs_u = st.slider('How many recs? for Age', 1, 20, 2)
+n_recs_u = st.slider('How many recs? for EducationField', 1, 20, 2)
 
-if st.button('Recommend Age - click!'):
-    similar_cities = similar_Monthly_Age(one_user, n_recs_u)
+if st.button('Recommend EducationField - click!'):
+    similar_cities = similar_Department_EducationField(one_user, n_recs_u)
     st.write(similar_cities)
 
