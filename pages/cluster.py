6459478fdf0c04833b51eb53799d03d72109d03d
Only-Mike <112564345+Only-Mike@users.noreply.github.com>
2022-10-04 20:42:46 +0200

    d

--- pages/cluster.py
+++ pages/cluster.py
@@ -63,7 +63,7 @@ vis_data['Gender'] = hr_df['Gender']
 vis_data['MonthlyIncome'] = hr_df['MonthlyIncome']
 vis_data.columns = ['x', 'y', 'Gender', 'MonthlyIncome']
 
-alt.Chart(vis_data).mark_circle(size=60).encode(
+st.altir_Chart(vis_data).mark_circle(size=60).encode(
     x='x',
     y='y',
     tooltip=['Gender', 'MonthlyIncome']
